8feb501e9bb462e0c12e7e601e852aa68b4d930e
Matskevich_Hanna <hankohanna@mail.ru>
2022-12-15 17:19:42 +0300

    задача на сумму цифр числа

--- lesson02/task01.py
+++ lesson02/task01.py
@@ -3,3 +3,12 @@
 # Пример:
 # 67.82 -> 23
 # (-0.56) -> 11
+
+number = input('Введите число: ')
+list_num = list(number)
+semple_list = []
+for i in range(0, len(list_num)):
+    if list_num[i].isdigit():
+        semple_list.append(int(list_num[i]))
+sum_numbers = sum(semple_list)
+print(number, '->', sum_numbers)
