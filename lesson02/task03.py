296b403e539740b4da33d646ed0563d1fb0a8d7b
Matskevich_Hanna <hankohanna@mail.ru>
2022-12-15 18:23:24 +0300

    задача на массив с нулями

--- lesson02/task03.py
+++ lesson02/task03.py
@@ -3,3 +3,17 @@
 # Пример:
 # - пусть N = 4, тогда [28, -46, 14, -14] => [28, -46, 0, 14, -14, 0]
 
+import random
+
+cnt = 0
+number = int(input('Введите число: '))
+
+rand = [random.randint(-100,100) for i in range(number)]
+
+for i in range(number):
+    if rand[i] < 0:
+        rand.insert(i + 1, cnt)
+if rand[-1] < 0:
+    rand.append(0)
+
+print(rand)
