e15bb846ea1641b2e563f55a6e56cf62c1736d78
Matskevich_Hanna <hankohanna@mail.ru>
2022-12-15 17:33:52 +0300

    задача на произведение чисел

--- lesson02/task02.py
+++ lesson02/task02.py
@@ -3,3 +3,11 @@
 # Не используйте функцию math.factorial.
 # Пример:
 # - пусть N = 4, тогда [ 1, 2, 6, 24 ] #(1, 1*2, 1*2*3, 1*2*3*4)
+
+number = int(input('Введите число: '))
+set_multiply = []
+previous_multiply = 1
+for i in range(1, number + 1):
+    set_multiply.append((previous_multiply)*i)
+    previous_multiply = previous_multiply * i
+print(f'N = {number} -> {set_multiply}')
