7026704f8bb09eebeb8a175a4e2ded2ed5a93031
Matskevich_Hanna <hankohanna@mail.ru>
2022-12-19 00:12:14 +0300

    библиотека для 5 задач

--- lesson03/task01.py
+++ lesson03/task01.py
@@ -2,3 +2,22 @@
 # Напишите программу, которая найдёт сумму элементов списка, стоящих на нечётной позиции.
 # Пример:
 # [2, 3, 5, 9, 3] -> на нечётных позициях элементы 3 и 9, ответ: 12
+
+def input_list():
+    numbers = input('Введит несколько чисел через пробел: ').split(" ")
+    return numbers
+
+def find_sum(data_list):
+    # what_find = input('Что ищем?')
+    result = []
+    for i in range(len(data_list)):
+        if i % 2 != 0:
+            result.append(i)
+        return result                 # return f'Индекс нечетного элемента -> {i}'
+
+elements = input_list()
+result_list = find_sum(elements)
+print(f'Элементы списка, стоящие на нечётной позиции: {result_list}')
+
+
+
