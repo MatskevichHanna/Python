d72b63de3c6daeccf62b15e4d079595fa170c2e1
Matskevich_Hanna <hankohanna@mail.ru>
2022-12-19 19:36:33 +0300

    задача на преобразование десятиичного числа

--- lesson03/task04.py
+++ lesson03/task04.py
@@ -6,3 +6,17 @@
 # 3 -> 11
 # 2 -> 10
 
+from function import give_int
+
+def int_to_binary(number: int) -> str:
+    binary = []
+    while number // 2 != 0:
+        binary.insert(0, number % 2)
+        number //= 2
+    binary.insert(0, number)
+    result = "".join(map(str, binary))
+    return result
+
+num = give_int('Введите натуральное число: ', 1)
+binary = int_to_binary(num)
+print(f'{num} => {binary}')
