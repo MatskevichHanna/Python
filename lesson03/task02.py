f659f6ca7389849b1f8d5169215ed014ac56b1b7
Matskevich_Hanna <hankohanna@mail.ru>
2022-12-19 17:28:42 +0300

    задача на произведение пар чисел списка

--- lesson03/task02.py
+++ lesson03/task02.py
@@ -4,3 +4,15 @@
 # [2, 3, 4, 5, 6] => [12, 15, 16];
 # [2, 3, 5, 6] => [12, 15]
 
+from function import give_int, random_list
+
+def pair_mult(data_list):
+    multiple = list()
+    for i in range(len(data_list) // 2 + len(data_list) % 2):
+        multiple.append(data_list[i] * data_list[-1 - i])
+    return multiple
+
+size = give_int('Введите длину списка: ', 1)
+numbers = random_list(size)
+print(numbers)
+print(f'Произведение пар чисел списка = {pair_mult(numbers)}')
