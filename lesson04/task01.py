40056cca511a7f05059c7a50692aa782c871508f
Matskevich_Hanna <hankohanna@mail.ru>
2022-12-21 23:42:07 +0300

    функции для задач

--- lesson04/task01.py
+++ lesson04/task01.py
@@ -3,3 +3,18 @@
 # N = 20 -> [2,5]
 # N = 30 -> [2, 3, 5]
 
+from function import give_int
+
+def find_simple_multiplie(num):
+   i = 2
+   multiplie = []
+   while i * i <= num:
+       while num % i == 0:
+           multiplie.append(i)
+           num = num / i
+       i = i + 1
+   if num > 1:
+       multiplie.append(num)
+   return multiplie
+
+print(find_simple_multiplie('Введите натуральное число:\n'))
