9eff634155d9a31c54ffb806c1b9567a8103e34d
Matskevich_Hanna <hankohanna@mail.ru>
2022-12-26 12:46:49 +0300

    задача на удаление слов из текста

--- lesson05/task01.py
+++ lesson05/task01.py
@@ -1,3 +1,16 @@
 # Напишите программу, удаляющую из текста все слова, содержащие заданную строку.
 # Пример:
 # Пугать ты галок пугай => заданная строка "пугай" => Пугать ты галок
+
+def delete_string(input_string: str) -> str:
+
+    '''
+    Функция удаляет из текста все слова, содержащие заданную строку
+    '''
+
+    delete = input('Какую строку вы хотите удалить? \n')
+    result_list = [i for i in input_string.split() if delete not in i]
+    return ' '.join(result_list)
+
+result = delete_string(input('Введите текст: \n'))
+print(result)
