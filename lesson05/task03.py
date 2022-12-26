307568bc11f804dd0450b283f973712827904238
Matskevich_Hanna <hankohanna@mail.ru>
2022-12-26 18:35:39 +0300

    задача на игру крестики-нолики

--- lesson05/task03.py
+++ lesson05/task03.py
@@ -6,11 +6,21 @@ from function05 import give_int
 from typing import List
 
 def field_creator(turns_list: List):
+
+    '''
+    Функция которая печатает поле
+    '''
+
     print(f'{turns_list[0]} {turns_list[1]} {turns_list[2]}')
     print(f'{turns_list[3]} {turns_list[4]} {turns_list[5]}')
     print(f'{turns_list[6]} {turns_list[7]} {turns_list[8]}')
 
 def turn_maker(player: List, turn_list: List) -> None:
+
+    '''
+    Функция которая отвечает за ходы игроков и алгоритм самой игры
+    '''
+
     pl = player[0]
     for i in range(len(turn_list) - 1):
         new_turn = 10
@@ -32,6 +42,11 @@ def turn_maker(player: List, turn_list: List) -> None:
             pl = player[0]
 
 def win_condition(turns_list: List, player: List) -> None:
+
+    '''
+    Функция которая определяет победителя
+    '''
+
     if turns_list[0] == turns_list[1] == turns_list[2] \
         or turns_list[3] == turns_list[4] == turns_list[5] \
         or turns_list[6] == turns_list[7] == turns_list[8] \
@@ -47,6 +62,6 @@ def win_condition(turns_list: List, player: List) -> None:
 turn = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 player_1 = input('Введите имя первого игрока: ')
 player_2 = input('Введите имя второго игрока: ')
-players = [[1, player_1], [player_2]]
+players = [[1, player_1], [2, player_2]]
 field_creator(turn)
 turn_maker(players, turn)
