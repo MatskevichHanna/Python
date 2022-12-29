b75e5167bd0120ae3e04a7a1356c8c895982d5f8
Matskevich_Hanna <hankohanna@mail.ru>
2022-12-29 18:22:28 +0300

    задача на позицию второго вхождения строки в списке

--- lesson06/task01.py
+++ lesson06/task01.py
@@ -5,3 +5,15 @@
 # список: ["йцу", "фыв", "ячс", "цук", "йцукен"], ищем: "йцу", ответ: -1
 # список: ["123", "234", 123, "567"], ищем: "123", ответ: -1
 # список: [], ищем: "123", ответ: -1
+
+from typing import List
+
+def find_second_index(list_strings: List[str], search_word: str) -> int:
+    list_indexes = [i for i, string in enumerate(list_strings) if search_word in string]
+    print(list_indexes)
+    try:
+        return f'Индекс второго вхождения = {list_indexes[1]} '
+    except:
+        return 'Не найдено'
+
+print(find_second_index(["qwe", "asd", "zxc", "qwe", "ertqwe"], 'qwe'))
