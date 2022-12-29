42e2f166f5b28f83e8202231d046dc4d454412b3
Matskevich_Hanna <hankohanna@mail.ru>
2022-12-29 18:51:37 +0300

    задача на произведение пар чисел в списке

--- lesson06/task02.py
+++ lesson06/task02.py
@@ -1,3 +1,13 @@
 # Найти произведение пар чисел в списке. 
 # Парой считаем первый и последний элемент, второй и предпоследний и т.д.
 
+from function06 import give_int, random_list
+from typing import List
+
+def pair_mult(data_list: List[int]) -> List[int]:
+    return[data_list[i] * data_list[-1 - i] for i in range(len(data_list) // 2 + len(data_list) % 2)]
+
+dt_list = random_list(5)
+print(dt_list)
+res = pair_mult(dt_list)
+print(res)
